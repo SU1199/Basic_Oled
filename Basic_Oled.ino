d7a136a1164c67ccb0da7b5bc46443263f10cb6d
Danish Joshi <danishjoshi111@hotmail.com>
2018-04-10 09:06:02 +0530

    Add files via upload

---
+++ Basic_Oled.ino
@@ -0,0 +1,26 @@
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont); // Font To display 
+  u8g.setPrintPos(0, 20); 
+  // call procedure from base class, http://arduino.cc/en/Serial/Print
+  u8g.print("Enter Your Text Here"); //Text to display 
+}
+
+void setup(void) {
+    Serial.begin(9600); // baud rate
+    Serial.flush(); // Empty Serial data
+}
+
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  delay(500);
+}
